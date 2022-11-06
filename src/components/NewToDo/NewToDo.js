c423784d9c844f91ec6fbe50376f48390faca441
Tomás Quintal <tomas.quintal@gmail.com>
2022-11-06 19:51:48 +0000

    vercel fix

--- src/components/NewToDo/NewToDo.js
+++ src/components/NewToDo/NewToDo.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react';
 import StorageContext from '../../context/storage-context';
 import classes from './NewToDo.module.css';
-import Button from '../../ui/Button';
+import Button from '../../UI/Button';
 
 function NewToDo() {
     const context = useContext(StorageContext);
